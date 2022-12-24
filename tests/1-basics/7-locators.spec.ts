9fe24fb8067c81c51b1f623efc25ca0a7a927702
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2022-12-23 20:54:29 -0600

    feat: locator examples

--- tests/1-basics/7-locators.spec.ts
+++ tests/1-basics/7-locators.spec.ts
@@ -1,16 +1,29 @@
 /*
-    ? Locators help us to map web elements to interact with our web application.
+    * Locators help us to map web elements to interact with our web application.
+    ? More at: https://playwright.dev/docs/locators#quick-guide
 */
-import { test, expect, type Page } from "@playwright/test";
+import { test} from "@playwright/test";
 
 test.describe("Locators", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("https://demoqa.com/text-box");
   });
 
-  test("By Placeholder", async ({ page }) => {
+  test("Locators Examples", async ({ page }) => {
     await page
       .getByPlaceholder("Full Name")
       .fill('Joan Esquivel');
+    await page
+      .locator('[type="email"]')
+      .fill('joan@test.com')
+    await page
+      .locator('//textarea[@placeholder="Current Address"]')
+      .fill('my current address')
+    await page
+      .locator('#permanentAddress')
+      .fill('my permanent address')
+    await page
+      .getByRole('button', {name: "submit"})
+      .click()
   });
 });
