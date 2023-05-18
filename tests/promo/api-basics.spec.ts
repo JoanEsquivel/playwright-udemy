f9e2d40b43943ebfffc5a0565a63bb51739ea16a
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2023-05-17 19:40:28 -0600

    test

--- tests/promo/api-basics.spec.ts
+++ tests/promo/api-basics.spec.ts
@@ -5,15 +5,15 @@ const completed = { completed: true }
 
 import { test, expect } from '@playwright/test'
 test.describe('API testing basics', () => {
-    test('should create a new user by API request', async ({ request }) => {
+    test('should create a new todo by API request', async ({ request }) => {
         const newTodoRes = await request.post('/todos', { data })
         expect(newTodoRes.status()).toEqual(201)
         expect(newTodoRes.statusText()).toEqual('Created')
         expect(await newTodoRes.json()).toEqual(data)
     })
     test('should get todo by API request', async ({ request }) => {
-        const updateTodoRes = await request.get(`/todos`)
-        const body = await updateTodoRes.json()
+        const getTodoRes = await request.get(`/todos`)
+        const body = await getTodoRes.json()
         expect(body).toContainEqual(data)
     })
     test('should update a todo by API request', async ({ request }) => {
@@ -24,7 +24,7 @@ test.describe('API testing basics', () => {
         expect(body.completed).toEqual(true)
         expect(body.title).toEqual('start learning about cloud')
     })
-    test('should delete an existing user by API request', async ({
+    test('should delete an existing todo by API request', async ({
         request,
     }) => {
         const deleteTodoRes = await request.delete(`/todos/${data.id}`)
